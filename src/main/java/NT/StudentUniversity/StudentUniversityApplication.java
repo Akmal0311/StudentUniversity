25094f9dd45801e44500412ed41ef7c50887d36b
Akmal <akmalkomiljonov26@gmail.com>
2022-07-05 17:12:42 +0500

    first commit

---
+++ src/main/java/NT/StudentUniversity/StudentUniversityApplication.java
@@ -0,0 +1,13 @@
+package NT.StudentUniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NT/StudentUniversity/StudentUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package NT.StudentUniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
